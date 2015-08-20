b49a7569f40976e66198a679c6a8429df0b476da
yesunsong <1042282500@qq.com>
2015-08-20 11:59:52 +0800

    1.工具类 增加 虚拟摇杆类

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -1,4 +1,5 @@
 #include "HelloWorldScene.h"
+#include "util/HRocker.h"
 
 USING_NS_CC;
 
@@ -22,44 +23,51 @@ bool HelloWorld::init() {
 	}
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
-	/////////////////////////////
-	// 2. add a menu item with "X" image, which is clicked to quit the program
-	//    you may modify it.
-	// add a "close" icon to exit the progress. it's an autorelease object
-	auto closeItem = MenuItemImage::create("CloseNormal.png",
-			"CloseSelected.png",
-			CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
-	closeItem->setPosition(
-			Vec2(
-					origin.x + visibleSize.width
-							- closeItem->getContentSize().width / 2,
-					origin.y + closeItem->getContentSize().height / 2));
-	// create menu, it's an autorelease object
-	auto menu = Menu::create(closeItem, NULL);
-	menu->setPosition(Vec2::ZERO);
-	this->addChild(menu, 1);
+//	/////////////////////////////
+//	// 2. add a menu item with "X" image, which is clicked to quit the program
+//	//    you may modify it.
+//	// add a "close" icon to exit the progress. it's an autorelease object
+//	auto closeItem = MenuItemImage::create("CloseNormal.png",
+//			"CloseSelected.png",
+//			CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+//	closeItem->setPosition(
+//			Vec2(
+//					origin.x + visibleSize.width
+//							- closeItem->getContentSize().width / 2,
+//					origin.y + closeItem->getContentSize().height / 2));
+//	// create menu, it's an autorelease object
+//	auto menu = Menu::create(closeItem, NULL);
+//	menu->setPosition(Vec2::ZERO);
+//	this->addChild(menu, 1);
+//
+//	/////////////////////////////
+//	// 3. add your codes below...
+//	// add a label shows "Hello World"
+//	// create and initialize a label
+//	auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf",
+//			24);
+//	// position the label on the center of the screen
+//	label->setPosition(
+//			Vec2(origin.x + visibleSize.width / 2,
+//					origin.y + visibleSize.height
+//							- label->getContentSize().height));
+//	// add the label as a child to this layer
+//	this->addChild(label, 1);
+//	// add "HelloWorld" splash screen"
+//	auto sprite = Sprite::create("HelloWorld.png");
+//	// position the sprite on the center of the screen
+//	sprite->setPosition(
+//			Vec2(visibleSize.width / 2 + origin.x,
+//					visibleSize.height / 2 + origin.y));
+//	// add the sprite as a child to this layer
+//	this->addChild(sprite, 0);
 
-	/////////////////////////////
-	// 3. add your codes below...
-	// add a label shows "Hello World"
-	// create and initialize a label
-	auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf",
-			24);
-	// position the label on the center of the screen
-	label->setPosition(
-			Vec2(origin.x + visibleSize.width / 2,
-					origin.y + visibleSize.height
-							- label->getContentSize().height));
-	// add the label as a child to this layer
-	this->addChild(label, 1);
-	// add "HelloWorld" splash screen"
-	auto sprite = Sprite::create("HelloWorld.png");
-	// position the sprite on the center of the screen
-	sprite->setPosition(
-			Vec2(visibleSize.width / 2 + origin.x,
-					visibleSize.height / 2 + origin.y));
-	// add the sprite as a child to this layer
-	this->addChild(sprite, 0);
+	// 不跟随触点
+	Sprite *spRocker = Sprite::create("JoyStickPoint.png"); //摇杆
+	Sprite *spRockerBG = Sprite::create("JoyStickBg.png"); //摇杆背景
+	HRocker* rocker = HRocker::create(Vec2(150.0f, 130.0f), 50.0f, spRocker,
+			spRockerBG, true); //创建摇杆
+	this->addChild((Node*) rocker); //摇杆添加到layer中
 
 	return true;
 }
